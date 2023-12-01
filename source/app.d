01f77a29b561b363971c53b25e6cfe40dd1a3297
Matthew Armbruster <maarmbruster@nationalindemnity.com>
2023-12-01 09:58:54 -0600

    Still trying to get azure to work

--- source/app.d
+++ source/app.d
@@ -2,6 +2,7 @@ import vibe.core.core : runApplication;
 import vibe.http.server;
 import vibe.d;
 import std.random : uniform;
+import std.process : environment;
 
 import models.enumsandresults;
 
@@ -31,16 +32,16 @@ void getResultAndWriteIt(in GameChoices userChoice, scope HTTPServerResponse res
 
 void main()
 {
-
+	immutable port = environment.get("FUNCTIONS_CUSTOMHANDLER_PORT", "8080").to!ushort;
 	// todo: write rock paper scissors into api and then look into azure
     // We are now using URLRouter here
     auto router = new URLRouter;
-    router.get("/", &rand);
-    router.get("/rock", &rock);
-    router.get("/paper", &paper);
-    router.get("/scissors", &scissors);
+    router.get("/api/HttpExample", &rand);
+    // router.get("/rock", &rock);
+    // router.get("/paper", &paper);
+    // router.get("/scissors", &scissors);
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = port;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 
 	auto l = listenHTTP(settings, router);
